7c2fd3f76037f516df562ff041d98cae3ae732aa
Mssivan27 <santhoshsivan.m@prodapt.com>
2022-07-20 12:15:33 +0530

    week assessment 1

---
+++ Java25/src/main/java/com/prodapt/basics/SchoolRecordsTester.java
@@ -0,0 +1,15 @@
+package com.prodapt.basics;
+
+public class SchoolRecordsTester {
+	public static void main(String[] args) {
+		SchoolRecords ob1 = new SchoolRecords();
+		ob1.displayRecords();
+		ob1.addRecords(111, "Sri Vijay Vidyalaya Matriculation", "Dharmapuri" );
+		ob1.addRecords(222, "Abacus Montesori", "Perungudi" );
+		ob1.addRecords(333, "British International", "Neelankarai" );
+		ob1.addRecords(444, "St.John Universal", "Pallvakam" );
+		ob1.addRecords(555, "St.Patricks", "Adyar" );
+		}
+
+
+}
---
+++ Java25/src/main/java/com/prodapt/basics/SiTester.java
@@ -0,0 +1,12 @@
+package com.prodapt.basics;
+
+public class SiTester {
+	public static void main(String[] args) {
+		SI.principle = 15000;
+		SI.rate = 10;
+		SI.time = 1;
+		SI.simpleInterest();
+		}
+
+
+}
