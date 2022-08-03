b906215a3816429f604efe7fcf1fb81c13890631
Mssivan27 <108717819+Mssivan27@users.noreply.github.com>
2022-08-03 17:31:02 +0530

    Create TesterBusiness.java

---
+++ week 3/TesterBusiness.java
@@ -0,0 +1,10 @@
+package com.prodapt.OOPbasics;
+
+public class Library { 
+	int no_of_books = 3;
+	 int n = no_of_books - 1;
+	 int id[] = {111 , 222 , 333};
+	 String name[] = {" The Guide ", " Malgudi Days ", " Godan"};
+	 String author[] = {"R.k. Narayan ", " R.K. Narayan ", " Munshi Premchand "};
+
+}
